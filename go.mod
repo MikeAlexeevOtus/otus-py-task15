15b25f4b14354055c34e67cdb5eaccd7f26ce3a9
mikhail <mike.42.alexeev@gmail.com>
2021-02-14 17:26:27 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module memcload
+
+go 1.15
+
+require (
+	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
+	google.golang.org/protobuf v1.25.0
+)
---
+++ memcload.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+
+    //"google.golang.org/protobuf"
+    //"github.com/bradfitz/gomemcache/memcache"
+)
+
+func main() {
+    fmt.Println("hello")
+}
